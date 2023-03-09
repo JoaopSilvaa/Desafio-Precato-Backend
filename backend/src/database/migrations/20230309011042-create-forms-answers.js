b0bee6aa9de3b8a274e46e5c72fc4b614c7040b2
Joao Antonio <joaopsilva.dev@gmail.com>
2023-03-08 22:25:18 -0300

    feat: :construction: criação de migration forms_answers

---
+++ backend/src/database/migrations/20230309011042-create-forms-answers.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('forms_answers', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('forms_answers')
+  }
+};
